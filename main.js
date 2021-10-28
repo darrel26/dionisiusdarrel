e7959e824970566bddf00a89b2b41a053cbbee0f
Dionisius Darrel <dion05.talenta@gmail.com>
2021-10-28 22:17:35 +0700

    adding-files

---
+++ main.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('is-active')
+    mobileMenu.classList.toggle('is-active')
+});
+
+
